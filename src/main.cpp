012240d3cfb75fbaf2218e35bcad41c11c9f5891
Rodolfo Lam Figueroa <r_lam@hotmail.com>
2020-05-09 02:24:24 -0500

    Refactor init code

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,15 @@
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_primitives.h"
 
-constexpr int SCREEN_H{ 400 };
-constexpr int SCREEN_W{ 224 };
+#include "main.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char const **argv)
 {
-	bool sane = al_init();
-	if(!sane){
-		std::cout << "ALLEGRO 5 FAILED TO INITIALIZE!\n";
-	}
-
-	ALLEGRO_DISPLAY *display = al_create_display(SCREEN_H, SCREEN_W);
-	if(nullptr == display) {
-		std::cout << "FATAL ERROR: display " << display << "\n";
-	}
-	ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
-	if(nullptr == queue) {
-		std::cout << "FATAL ERROR: queue " << queue << "\n";
-	}
-	ALLEGRO_TIMER *timer = al_create_timer(1.0/60.0);
-	if(nullptr == timer) {
-		std::cout << "FATAL ERROR: timer " << timer << "\n";
-	}
-
-	al_install_keyboard();
-	al_init_image_addon();
-	al_init_primitives_addon();
-
-	al_register_event_source(queue, al_get_display_event_source(display));
-	al_register_event_source(queue, al_get_keyboard_event_source());
-	al_register_event_source(queue, al_get_timer_event_source(timer));
+	startSystems();
 
 	al_start_timer(timer);
+
+	int frames_skipped{ 0 };
 
 	bool running { true };
 	bool draw { false };
@@ -55,11 +32,14 @@ int main(int argc, char const *argv[])
 				running = false;
 				break;
 			case ALLEGRO_EVENT_TIMER:
-				draw = true;
+				if (frames_skipped++ <= MAX_FRAME_SKIPS) // DOWBLECHECK THIS!
+				{
+					draw = true;
+				}
 				break;
 		}
 
-		if((draw == true) && al_is_event_queue_empty(queue)){
+		if(draw && al_is_event_queue_empty(queue)){
 			draw = false;
 
 			ALLEGRO_COLOR color = al_map_rgb_f(1.0f, 0.0f, 1.0f);
@@ -69,4 +49,39 @@ int main(int argc, char const *argv[])
 
 	}
 	return 0;
+}
+
+inline void must_init(bool subsystem, const char* message)
+{
+	if (!subsystem) {
+		std::cerr << message << "\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+inline void* must_exist(void* val, const char* message)
+{
+	if (!val) {
+		std::cerr << message << "\n";
+		exit(EXIT_FAILURE);
+	}
+
+	return val;
+}
+
+void startSystems()
+{
+	must_init(al_init(), "ALLEGRO 5 FAILED TO INITIALIZE!");
+
+	display = (ALLEGRO_DISPLAY*)must_exist(al_create_display(SCREEN_H, SCREEN_W), "FATAL ERROR: display ");
+	queue = (ALLEGRO_EVENT_QUEUE*)must_exist(al_create_event_queue(), "FATAL ERROR: queue ");
+	timer = (ALLEGRO_TIMER*)must_exist(al_create_timer(1.0 / FPS), "FATAL ERROR: timer ");
+
+	must_init(al_install_keyboard(), "KEYBOARD DRIVER ERROR!");
+	must_init(al_init_image_addon(), "IMAGE ADD-ON ERROR!");
+	must_init(al_init_primitives_addon(), "PRIMITIVES ADD-ON ERROR!");
+
+	al_register_event_source(queue, al_get_display_event_source(display));
+	al_register_event_source(queue, al_get_keyboard_event_source());
+	al_register_event_source(queue, al_get_timer_event_source(timer));
 }
---
+++ src/main.h
@@ -0,0 +1,19 @@
+#pragma once
+
+constexpr int MAX_FRAME_SKIPS{ 3 };
+constexpr double FPS{ 60.0 };
+constexpr int SCREEN_H{ 400 };
+constexpr int SCREEN_W{ 224 };
+/* Remember to limmit content to within 4px top-bottom
+   and 8px within left-right.
+
+   If we multiply this resolution by 5 we get a nice
+   approximation to 1920x1080, with some overscan.*/
+
+ALLEGRO_DISPLAY* display{ nullptr };
+ALLEGRO_EVENT_QUEUE* queue{ nullptr };
+ALLEGRO_TIMER* timer{ nullptr };
+
+inline void must_init(bool subsystem, const char* message);
+inline void* must_exist(void* val, const char* message);
+void startSystems();
